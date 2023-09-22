ef47b879c30847a7f089baf133ba52189654c1b7
Rachid-Ait-Rhoudane <rachid.aitrhoudane@etu.uae.ac.ma>
2023-09-22 15:36:56 +0100

    add new animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./*.html", "./public/js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "bounce-slow": "bounce_slow 3s infinite",
+      },
+      keyframes: {
+        bounce_slow: {
+          "0%, 100%": {
+            "transform": "translateY(-25%)",
+            "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)"
+          },
+          "50%" : {
+            "transform": "translateY(0)",
+            "animation-timing-function": "cubic-bezier(0, 0, 0.2, 1)"
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
