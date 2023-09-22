bb1d2087e0793929998633ff8f3b616a083e5ed6
Rachid-Ait-Rhoudane <rachid.aitrhoudane@etu.uae.ac.ma>
2023-09-22 22:46:59 +0100

    finishing the special header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       animation: {
         "bounce-slow": "bounce_slow 3s infinite",
+        "heading-before":"heading_before 500ms linear both",
+        "heading-after":"heading_after 500ms linear both",
       },
       keyframes: {
         bounce_slow: {
@@ -16,7 +18,32 @@ module.exports = {
             "transform": "translateY(0)",
             "animation-timing-function": "cubic-bezier(0, 0, 0.2, 1)"
           }
+        },
+        heading_before: {
+          "50%": {
+            "left": "0",
+            "border-radius": "0"
+          },
+          "100%": {
+            "left": "0",
+            "border-radius": "0",
+            "height": "100%",
+            "width": "50%"
+          }
+        },
+        heading_after: {
+          "50%": {
+            "right": "0",
+            "border-radius": "0"
+          },
+          "100%": {
+            "right": "0",
+            "border-radius": "0",
+            "height": "100%",
+            "width": "50%"
+          }
         }
+
       }
     },
   },
